b9acbe5a2399a3cb5b17245305fd79d9d151a72f
Kym McInerney <kym@holodigm.com.au>
2014-07-27 00:28:18 +1000

    simple bot with range finder collision avoidance

--- examples/ping.js
+++ examples/ping.js
@@ -5,10 +5,10 @@ board.on("ready", function() {
 
     //Create new Ping and show distance on change
     var ping = new five.Ping(7);
-
+    console.log('HERE');
+    console.log(ping);
     ping.on("change", function( err, value ) {
-
         console.log('Object is ' + this.cm + ' cm away');
         console.log('Object is ' + this.inches + ' inches away');
     });
-});  
+});
--- examples/simplebot.js
+++ examples/simplebot.js
@@ -7,15 +7,15 @@ var five = require("johnny-five");
 var keypress = require('keypress');
 
 var STOPVAL = 90;
-var RSTOPVAL = 88;
-var LSTOPVAL = 89;
+var RSTOPVAL = 94;
+var LSTOPVAL = 95;
 
 var port = process.argv[2] || "";
 
 var opts = {};
 
 if (port != "") {
-    opts.port = port;
+  opts.port = port;
 }
 
 keypress(process.stdin);
@@ -25,71 +25,83 @@ var board = new five.Board(opts);
 
 board.on("ready", function() {
 
-    console.log("Welcome to SimpleBot!")
-    console.log("Control the bot with the arrow keys, and SPACE to stop.")
+  console.log("Welcome to SimpleBot!")
+  console.log("Control the bot with the arrow keys, and SPACE to stop.")
 
-    var led = new five.Led(5);
-    var left_wheel  = new five.Servo({ pin:  9, type: 'continuous' }).to(LSTOPVAL);
-    var right_wheel = new five.Servo({ pin: 10, type: 'continuous'  }).to(RSTOPVAL);
+  var led = new five.Led(5);
+  var left_wheel  = new five.Servo({ pin:  9, type: 'continuous' }).to(LSTOPVAL);
+  var right_wheel = new five.Servo({ pin: 10, type: 'continuous'  }).to(RSTOPVAL);
 
-    process.stdin.resume(); 
-    process.stdin.setEncoding('utf8'); 
-    process.stdin.setRawMode(true); 
+  //Create new Ping and show distance on ch  ange
+  var ping = new five.Ping(7);
+  console.log('Initialising Range Finder');
+  ping.on("change", function( err, value ) {
 
-    process.stdin.on('keypress', function (ch, key) {
-    
+    if (this.cm < 10) {
+      console.log('WARNING: Collision avoidance activated at : ' + this.cm + ' cm away');
+      left_wheel.to(LSTOPVAL);
+      right_wheel.to(RSTOPVAL);
+    }
+  });
+
+  process.stdin.resume();
+  process.stdin.setEncoding('utf8');
+  process.stdin.setRawMode(true);
+
+  process.stdin.on('keypress', function (ch, key) {
+
     if ( !key ) return;
 
     if ( key.name == 'q' ) {
 
-        console.log('Quitting');
-        process.exit();
+      console.log('Quitting');
+      process.exit();
 
     } else if ( key.name == 'up' ) {
 
-        console.log('Forward');
-        left_wheel.ccw();
-        right_wheel.cw();
+      console.log('Forward');
+      left_wheel.ccw();
+      right_wheel.cw();
 
     } else if ( key.name == 'down' ) {
 
-        console.log('Backward');
-        left_wheel.cw();
-        right_wheel.ccw();      
+      console.log('Backward');
+      left_wheel.cw();
+      right_wheel.ccw();
 
     } else if ( key.name == 'left' ) {
 
-        console.log('Left');
-        left_wheel.cw();
-        right_wheel.cw();      
+      console.log('Left');
+      left_wheel.cw();
+      right_wheel.cw();
 
     } else if ( key.name == 'right' ) {
 
-        console.log('Right');
-        left_wheel.ccw();
-        right_wheel.ccw();
+      console.log('Right');
+      left_wheel.ccw();
+      right_wheel.ccw();
 
     } else if ( key.name == 'space' ) {
 
-        console.log('Stopping');
-        left_wheel.to(LSTOPVAL);
-        right_wheel.to(RSTOPVAL);
+      console.log('Stopping');
+      left_wheel.to(LSTOPVAL);
+      right_wheel.to(RSTOPVAL);
 
     } else if (key.name == "o") {
-        console.log("on");
-        led.on();
+      console.log("on");
+      led.on();
     } else if (key.name == "f") {
-        console.log("off");
-        led.off();
+      console.log("off");
+      led.off();
     } else if (key.name == "s") {
-        console.log("blink");
-        led.strobe(2000);
+      console.log("blink");
+      led.strobe(2000);
     }
   });
 });
 
 board.on("error", function(err) {
-    console.log(err.message);
-    process.exit();
+  console.log(err.message);
+  process.exit();
 });
 
